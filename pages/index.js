de7c5a3a459f967b167ca3ed94164d06dcabc9ab
wander <wander@mit.edu>
2025-11-22 13:56:22 -0800

    feat: continuing tutorial

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      
       <main>
         <h1 className={styles.title}>
           Read <Link href="/posts/first-post">this page!</Link>
