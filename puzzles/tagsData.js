efdde783d8a8c8d2153c3c2db78d4672a1b8c0ba
wander <wander@mit.edu>
2026-05-04 20:28:33 -0700

    push a batch of puzzles

--- puzzles/tagsData.js
+++ puzzles/tagsData.js
@@ -1,5 +1,5 @@
-export const sizes = ["10x10", "10x18"];
-export const genres = ["akari", "fillomino", "nurikabe", "slalom", "slitherlink"];
+export const sizes = ["5x5", "7x7", "10x10", "10x18"];
+export const genres = ["akari", "cave", "fillomino", "masyu", "nurikabe", "pentopia", "skyscrapers", "slalom", "slitherlink", "statuepark", "tomtom", "yajilin", "yinyang"];
 export const sources = ["blog"]
-export const others = ["variant"]
+export const others = ["variant", "favorites"]
 export const tagsData = sizes.concat(genres).concat(sources).concat(others);
