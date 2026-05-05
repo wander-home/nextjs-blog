402637ca242e3d4cc6e7da8794a3157d12d81201
wander <wander@mit.edu>
2026-05-04 17:28:17 -0700

    update header

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
  
-const name = 'Your Name';
-export const siteTitle = 'Next.js Sample Website';
+const name = "wander's puzzles";
+export const siteTitle = 'wander';
  
 export default function Layout({ children, home }) {
   return (
@@ -14,23 +14,16 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content="wander's personal website"
         />
         <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header className={styles.header}>
         {home ? (
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src="/images/blobhajreach.png"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
@@ -43,7 +36,7 @@ export default function Layout({ children, home }) {
             <Link href="/">
               <Image
                 priority
-                src="/images/profile.jpg"
+                src="/images/blobhajreach.png"
                 className={utilStyles.borderCircle}
                 height={108}
                 width={108}
