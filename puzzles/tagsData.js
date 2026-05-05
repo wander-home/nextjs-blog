037f2a8b550ca42258a3f866738e5eeb88e7ba52
wander <wander@mit.edu>
2026-05-04 17:18:12 -0700

    add some more puzzles

--- puzzles/tagsData.js
+++ puzzles/tagsData.js
@@ -1,1 +1,4 @@
-export const tagsData = ["10x10", "fillomino", "slitherlink", "nurikabe"];
+const sizes = ["10x10"];
+const genres = ["akari", "fillomino", "nurikabe", "slalom", "slitherlink"];
+const sources = ["blog"]
+export const tagsData = sizes.concat(genres).concat(sources);
