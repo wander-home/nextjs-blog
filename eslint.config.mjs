4f5709754b67a81cb3c96d42eec0891484e5d123
wander <wander@mit.edu>
2026-05-05 21:46:15 -0700

    adding prettier in progress

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,25 @@ import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
+import prettierConfig from 'eslint-plugin-prettier/recommended'; 
+import { globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} }, settings: {react: {version: "19"}}},
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: [js.configs.recommended, prettierConfig], languageOptions: { globals: {...globals.browser, ...globals.node} }, settings: {react: {version: "19"}}},
   pluginReact.configs.flat.recommended,
   { rules: {
     "react/prop-types": "off",
     "react/react-in-jsx-scope": "off",
+    'prettier/prettier': [
+      'error', 
+      { 
+        singleQuote: true,
+        printWidth: 100,
+        tabWidth: 2,
+        semi: true,
+        trailingComma: 'es5',
+        bracketSpacing: true,
+        endOfLine: 'lf'
+      }],
   }}
 ]);
