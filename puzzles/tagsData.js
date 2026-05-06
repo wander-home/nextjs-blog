c23b38937ca00adcf8da8755c00463cb7c3a44c5
wander <wander@mit.edu>
2026-05-05 21:02:22 -0700

    add a bunch more puzzles

--- puzzles/tagsData.js
+++ puzzles/tagsData.js
@@ -1,5 +1,5 @@
-export const sizes = ["5x5", "7x7", "10x10", "10x18"];
-export const genres = ["akari", "cave", "fillomino", "masyu", "nurikabe", "pentopia", "skyscrapers", "slalom", "slitherlink", "statuepark", "tomtom", "yajilin", "yinyang"];
+export const sizes = ["5x5", "7x7", "10x10", "10x18", "12x12"];
+export const genres = ["akari", "cave", "compass", "dominodivide", "doppelblock", "fillomino", "herugolf", "kropki", "lits", "masyu", "mochikoro", "nurikabe", "pentopia", "skyscrapers", "slalom", "slitherlink", "slovaksums", "starbattle", "statuepark", "tapa", "tapalikeloop", "tomtom", "wittgensteinbriquet", "yajilin", "yinyang"];
 export const sources = ["blog"]
 export const others = ["variant", "favorites"]
 export const tagsData = sizes.concat(genres).concat(sources).concat(others);
